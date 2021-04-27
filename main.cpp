f47b82ed34c18b723ebecdf1dc43d7827a73339b
tehjeipi <72479272+tehjeipi@users.noreply.github.com>
2021-04-27 16:20:44 -0500

    Pruebas de Persona

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include "Persona.h"
 
 using namespace std;
 
 int main(){
-  cout<<"hola mundo"<<endl;
-  cout<<"prueba2"<<endl;
+  Persona p1("Juan Pablo","Tabachines",333669810,"23/09/02","H");
+  cout<<p1.getNombre()<<" "<<p1.getDireccion()<<" "<<p1.getNumero()<<" "<<p1.getNacimiento()<<" "<<p1.getSexo()<<endl;
 
-  cout<<"prueba2"<<endl;
   return 0;
 }
