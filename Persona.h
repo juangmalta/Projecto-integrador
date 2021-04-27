0e030de7891471a2c2551fcf0124df5ef14322b5
tehjeipi <72479272+tehjeipi@users.noreply.github.com>
2021-04-27 15:52:40 -0500

    No se que tenia pero volvi a hacer la clase y ya sirve

--- Persona.h
+++ Persona.h
@@ -1,20 +1,19 @@
 #ifndef PERSONA_H_INCLUDED
 #define PERSONA_H_INCLUDED
+
 #include <string>
 
-class Persona{
-protected:
-	string nom;
-	string dire; //confused sounds porque no se marca como string?
-	int telefono;
-	int nac;
-	string sexo;
-	int rfc; //No segura porque solo afecta a clientes (se crearía un nuevo .h para ellos?)
-public:
-	Persona();
-	Persona(string nom, string dire, int telefono, Fecha nac, string sexo);
-	getNom(); //Al rato completo, tengo clase xd
-	getDire();//No se como funciona github, cualquier error ahorita lo arreglo
+using namespace std;
 
-}
+class Persona
+{
+protected:
+	string nombre;
+	string direccion;
+	int numero;
+	string nacimiento;
+	string sexo;
+
+};
+
 #endif
