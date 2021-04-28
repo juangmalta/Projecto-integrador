2f2c78e1aa846e1b47c39ea1b17d1d49e31c53b7
A01639716 <A01639716@itesm.mx>
2021-04-28 11:03:04 -0500

    Update ProductoOrden.h

--- ProductoOrden.h
+++ ProductoOrden.h
@@ -1,14 +1,25 @@
 #ifndef PRODUCTO_ORDEN_H_INCLUDED
 #define PRODUCTO_ORDEN_H_INCLUDED
-
+#include <string>
+using namespace std;
 #include "Orden.h"
 
 class ProductoOrden:public Orden
 {
-    protected:
+protected:
+    int codprod;
+    string nombrprod;
+    int precCompra;
+    int precVenta;
+    int cantprodex;
+    int peso;
+    int tamañoEmpaque;
+
+private:
+    ProductoOrden();
+    ProductoOrden(int,string,int,int,int,int,int);
 
 
-    private:
 
 };
 
