9a80138ed2a99fe3eb001093c3218a25177f0177
A01639716 <A01639716@itesm.mx>
2021-04-28 10:53:19 -0500

    Create ProductoOrden.h

---
+++ ProductoOrden.h
@@ -0,0 +1,18 @@
+#ifndef PRODUCTO_ORDEN_H_INCLUDED
+#define PRODUCTO_ORDEN_H_INCLUDED
+
+#include "Orden.h"
+
+class ProductoOrden:public Orden
+{
+    protected:
+
+
+    private:
+
+};
+
+
+
+
+#endif
