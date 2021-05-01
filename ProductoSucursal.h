d508bc41e2ffcc1ab1864c120a191c13ee20091b
MasterCraft117 <master.craft117@gmail.com>
2021-05-01 16:34:56 -0500

    Create ProductoSucursal.h

---
+++ ProductoSucursal.h
@@ -0,0 +1,30 @@
+#define PRODUCTOSUCURSAL_H_INCLUDED
+#ifndef PRODUCTOSUCURSAL_H_INCLUDED
+#include <string>
+
+class ProductoSucursal
+{
+private:
+  int CodigoProducto;
+  string NombreProducto;
+  int PrecioCompra;
+  int PrecioVenta;
+  int Existencias[];
+  double Peso;
+  double Tamano;
+
+public:
+  int getCodigoProducto();
+  string getNombreProducto();
+  int getPrecioCompra();
+  int getPrecioVenta();
+  int getExistencias(int);
+  double getPeso();
+  double getTamano();
+  void imprime();
+}
+
+
+
+
+#endif
