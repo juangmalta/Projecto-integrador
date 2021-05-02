0d37ebf31d4bcdf761f44276bc2193e6a7b72e8b
tehjeipi <72479272+tehjeipi@users.noreply.github.com>
2021-05-01 20:26:11 -0500

    Correcciones a producto sucursal

--- ProductoSucursal.cpp
+++ ProductoSucursal.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <string>
 #include "ProductoSucursal.h"
@@ -62,59 +61,3 @@ void ProductoSucursal::imprime(int i){
 	cout<<"Peso: "<<this->Peso<<endl;
 	cout<<"Tamaño: "<<this->Tamano<<endl;
 }
-=======
-#include "ProductoSucursal.h"
-#include <iostream>
-using namespace std;
-
-
-ProductoSucursal::ProductoSucursal(int cod, string nom, int prec, int precc, int precv, int cant, double pes, double tam) {
-    codprod = cod;
-    nombrprod = nom;
-    precio = prec;
-    precCompra = precc;
-    precVenta = precv;
-    existencia = cant;
-    peso = pes;
-    tamañoEmpaque = tam;
-}
-
-int ProductoSucursal::getCodigoProducto() {
-    return codprod;
-}
-
-int ProductoSucursal::getNombreProducto() {
-    return nombrprod;
-}
-
-int ProductoSucursal::getPrecioCompra() {
-    return precCompra;
-}
-
-int ProductoSucursal::getPrecioVenta() {
-    return precVenta;
-}
-
-int ProductoSucursal::getExistencia() {
-    return existencia;
-}
-
-double ProductoSucursal::getPeso() {
-    return peso;
-}
-
-double ProductoSucursal::getTamaño() {
-    return tamaño;
-}
-
-void ProductoSucursal::imprimir() {
-    cout << "Codigo de producto: " << codprod << endl;
-    cout << "Nombre de producto: " << nombrprod << endl;
-    cout << "Precio de producto: " << precio << endl;
-    cout << "Precio(Compra) de producto: " << precCompra << endl;
-    cout << "Precio(Venta) de producto: " << precVenta << endl;
-    cout << "Existencias de producto: " << existencia << endl;
-    cout << "Peso de producto: " << peso << endl;
-    cout << "Tamaño de prodcuto: " << tamaño << endl;
-}
->>>>>>> 48d342b74fc8ea26558245a75a9cbd3ec4b9d072
