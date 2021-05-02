6fb956109b57313848f74110fc53e1b8b6f59f1d
A01639716 <83232239+A01639716@users.noreply.github.com>
2021-05-01 20:27:01 -0500

    Add files via upload

---
+++ ProductoOrden.cpp
@@ -0,0 +1,27 @@
+#include "ProductoOrden.h"
+#include <iostream>
+
+using namespace std;
+
+ProductoOrden::ProductoOrden(){
+    codprod=0;
+    nombrprod="";
+    precCompra=0;
+    precVenta=0;
+    cantprodex=0;
+    peso=0;
+    tamañoEmpaque=0;
+}
+ProductoOrden::ProductoOrden(int _codprod,string _nomprod,int _precCompra,int _precVenta, int _cantprodex,int _peso, int _tamañoEmpaque):Orden(){
+    codprod=_codprod;
+    nombrprod=_nomprod;
+    precCompra=_precCompra;
+    precVenta=_precVenta;
+    cantprodex=_cantprodex;
+    peso=_peso;
+    tamañoEmpaque=_tamañoEmpaque;
+}
+
+string ProductoOrden::getCliente(){
+    
+}
