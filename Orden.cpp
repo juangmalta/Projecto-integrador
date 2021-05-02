af9304291fc7f711490ef402f861d8dc199b78e5
A01639716 <A01639716@itesm.mx>
2021-05-01 23:52:33 -0500

    Update Orden.cpp

--- Orden.cpp
+++ Orden.cpp
@@ -1,16 +1,30 @@
 #include "Orden.h"
 
-//Orden::Orden(){
+#include <iostream>
 
-//}
+using namespace std;
 
-//Orden::Orden(ProductoSucursal *arr){
+Orden::Orden(){
+  arrprod = new ProductoSucursal[1];
+  empleado = Empleado();
+  cliente = Cliente();
+  size=0;
+}
 
-//arrprod = arr;
-//}
+Orden::Orden(int t,ProductoSucursal *arr,Cliente c,Empleado e){
+arrprod = arr;
+empleado = e;
+cliente = c;
+size = t;
+}
 
 void Orden::print(){
+    cout << "Orden "<<" le atendio " << endl;
+    empleado.imprimir();
+    cout << "Cliente "<<endl;
+    cliente.imprimir();
+
     
-    arrprod->imprime(20);
+    arrprod->imprime(arrprod->getTamano());
     
 }
